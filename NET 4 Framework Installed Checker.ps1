2d9d27bec31037cca015c270d489f88d222dc6c3
AustinWay <31394469+AustinWay@users.noreply.github.com>
2020-06-25 11:22:39 -0500

    Update NET 4 Framework Installed Checker.ps1

--- NET 4 Framework Installed Checker.ps1
+++ NET 4 Framework Installed Checker.ps1
@@ -1,6 +1,6 @@
 <#
 Program: .NET 4 Framework Installed Checker 
-Description: Checks if .NET 4.5 Framework or higher is installed on a local machine 
+Description: Powershell script that checks if .NET 4.5 Framework or higher is installed on a local machine 
 Author: Austin Way
 Date: 6/25/2020
 #>
